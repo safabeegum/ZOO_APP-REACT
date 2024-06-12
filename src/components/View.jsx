dcb38a838b9fc4bf8f7dc12a4fc7aedd55fd365e
safabeegum <pasafabeegum@gmail.com>
2024-06-13 00:18:51 +0530

    Completed

---
+++ src/components/View.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table table-bordered border-primary">
+                <thead>
+    <tr>
+      <th scope="col">ANIMAL ID</th>
+      <th scope="col">ANIMAL NAME</th>
+      <th scope="col">DESCRIPTION</th>
+      <th scope="col">TYPE</th>
+      <th scope="col">CLASS TYPE</th>
+      <th scope="col">CAGE ID</th>
+      <th scope="col">AGE</th>
+      <th scope="col">DATE OF ARRIVAL</th>
+      <th scope="col">CARETAKER</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+    
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+</table>
+                </div>
+            </div>
+
+            </div>
+            </div>
+  )
+}
+
+export default View
