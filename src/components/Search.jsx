6defdbbf95a2a5141d8e7404197564f7c976f490
safabeegum <pasafabeegum@gmail.com>
2024-06-13 02:17:29 +0530

    Completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -54,11 +54,55 @@ const Search = () => {
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <button className="btn btn-primary" onClick={readValue}>SEARCH</button>
                         </div>
-                    </div>
+                    </div><br></br>
+
+{/* table design */}
+<div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table table-bordered border-primary">
+                <thead>
+    <tr>
+      <th scope="col">ANIMAL ID</th>
+      <th scope="col">ANIMAL NAME</th>
+      <th scope="col">DESCRIPTION</th>
+      <th scope="col">TYPE</th>
+      <th scope="col">CLASS TYPE</th>
+      <th scope="col">CAGE ID</th>
+      <th scope="col">AGE</th>
+      <th scope="col">DATE OF ARRIVAL</th>
+      <th scope="col">CARETAKER</th>
+    </tr>
+  </thead>
+  {
+    data.map(
+        (value,index) => {
+            return <tbody>
+            <tr>
+              
+                            <td>{value.animalid}</td>
+                            <td>{value.name}</td>
+                            <td>{value.description}</td>
+                            <td>{value.type}</td>
+                            <td>{value.ctype}</td>
+                            <td>{value.cageid}</td>
+                            <td>{value.age}</td>
+                            <td>{value.date}</td>
+                            <td>{value.caretaker}</td>
+                          </tr>
+                        </tbody>
+        }
+    )
+  }
+  
+</table>
+                </div>
+            </div>
+
+            </div>
                 </div>
             </div>
         </div>
-    </div>
+    
   )
 }
 
