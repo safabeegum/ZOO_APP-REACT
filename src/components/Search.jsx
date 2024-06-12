a8c29ec9f55203d28e05f836d32633b8b23ea40e
safabeegum <pasafabeegum@gmail.com>
2024-06-13 02:16:16 +0530

    Completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,43 @@ import Navbar from './Navbar'
 import axios from 'axios'
 
 const Search = () => {
-   
+    const[data,setData]=useState(
+        {
+            "animalid":"",
+        }
+    )
+
+    const [result,setresult] = useState([])
+    
+    const inputHandler=(event) => {
+        setData({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValue=() => {
+        console.log(data)
+        axios.post("",data).then(
+            (response) => {
+                setresult(response.data)
+            }
+        )
+    }
+
+        const deleteCourse=(id) => {
+            let input = {"_id": id}
+            axios.post("",input).then(
+                (response) => {
+                    console.log(response.data)
+                    if(response.data.status=="success")
+                        {
+                            alert("Successfully Deleted")
+                        }
+                        else
+                        {
+                            alert("Error")
+                        }
+                }
+            ).catch().finally()
+        }
   return (
     <div>
         <Navbar/>
@@ -12,11 +48,11 @@ const Search = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label htmlFor="" className="form-label">ANIMAL NO</label>
-                            <input type="text" className="form-control" />
+                            <label htmlFor="" className="form-label">ANIMAL ID</label>
+                            <input type="text" className="form-control" name='animalid' value={data.animalid} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-primary">SEARCH</button>
+                            <button className="btn btn-primary" onClick={readValue}>SEARCH</button>
                         </div>
                     </div>
                 </div>
