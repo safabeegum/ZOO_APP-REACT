0aeb9f97c1dec368ce7851f528b8b72ee9108e3e
safabeegum <pasafabeegum@gmail.com>
2024-06-13 01:55:06 +0530

    Completed

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -43,27 +43,27 @@ const Add = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">ANIMAL ID</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='animalid' value={data.animalid} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">ANIMAL NAME</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">DESCRIPTION</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea name="description" id="" className="form-control" value={data.description} onChange={inputHandler}></textarea>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">TYPE</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='type' value={data.type} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">CLASS TYPE</label>
-                            <select name="" id="" className="form-control">
+                            <select name="ctype" id="" className="form-control" value={data.ctype} onChange={inputHandler}>
                                 <option value="Mammal">Mammal</option>
                                 <option value="Bird">Bird</option>
                                 <option value="Reptile">Reptile</option>
@@ -76,22 +76,22 @@ const Add = () => {
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">CAGE ID</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='cageid' value={data.cageid} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">AGE</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='age' value={data.age} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">DATE OF ARRIVAL</label>
-                            <input type="date" name="" id="" className="form-control" />
+                            <input type="date" name="date" id="" className="form-control" value={data.date} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">CARETAKER</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='caretaker' value={data.caretaker} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
